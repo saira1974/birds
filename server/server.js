47fdaf5bb493a0333cd17c39d39c5744ef316b81
saira richardson <leedslawyers@gmail.com>
2021-11-08 08:18:48 +0000

    first commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true })
+  .then((client) => {
+    const db = client.db('birds');
+    const sightingsCollection = db.collection('sightings');
+    const sightingsRouter = createRouter(sightingsCollection);
+    app.use('/api/sightings', sightingsRouter);
+  })
+  .catch(console.err);
+
+app.listen(5001, function () {
+  console.log(`Listening on port ${ this.address().port }`);
+});
